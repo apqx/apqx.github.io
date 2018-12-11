65808f92a5ba926002f92800c1ce454fcdac07b2
apqx <changgongapq@gmail.com>
2018-12-11 23:36:04 +0800

    优化UI

--- js/init.js
+++ js/init.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   $('.slider').slider();
   $('.scrollspy').scrollSpy();
   $('.carousel').carousel();
+  $('.tooltipped').tooltip();
   hljs.initHighlightingOnLoad();
 });
 
