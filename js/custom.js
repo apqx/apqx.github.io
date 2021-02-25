db009e0d40e77f41efa3977f18cff7c45ef88336
apqx <changgongapq@gmail.com>
2021-02-26 07:55:33 +0800

    Upload 1 essay, add url encoder page
    1.央视、戏剧、4K、HEVC，惊喜连连

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,11 @@ const scrollToTop = () => {
         window.requestAnimationFrame(scrollToTop);
         window.scrollTo(0, c - c / 8);
     } else {
-        M.toast({html: '🐸'})
+        // M.toast({html: '🐸'})
     }
+};
+
+const copyUrl = () => {
+    // 截取、处理当前URL中的中文，encode后复制到剪切板
+    // M.toast({html: '🐸'})
 };
