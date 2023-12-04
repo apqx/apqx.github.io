d583ce6ffd1368a0cd1010e6690344225e0021b1
apqx <changgongapq@gmail.com>
2023-12-04 23:19:27 +0800

    Use new index with cover

--- npm/src/script/main.ts
+++ npm/src/script/main.ts
@@ -100,7 +100,7 @@ function initViews() {
         new MDCRipple(ele)
     }
 
-    for (const ele of document.querySelectorAll(".index-card,.grid-index-card")) {
+    for (const ele of document.querySelectorAll(".index-card,.grid-index-card,.index-top-card")) {
         new MDCRipple(ele)
     }
 
