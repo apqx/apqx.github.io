a70591c36301218c8630f1a40f10660afb3e9472
apqx <changgongapq@gmail.com>
2026-02-07 02:41:56 +0800

    Share dialog supports copy url to clipboard

--- npm/src/component/dialog/ShareDialog.tsx
+++ npm/src/component/dialog/ShareDialog.tsx
@@ -4,6 +4,8 @@ import type { BasicDialogProps } from "./BasicDialog"
 import { consoleDebug } from "../../util/log"
 import React from "react"
 import QRCodeStyling from "qr-code-styling"
+import { showSnackbar } from "../react/Snackbar"
+import { IconButton } from "../react/Button"
 
 interface ShareDialogStats {
     title: string,
@@ -14,16 +16,17 @@ class ShareDialog extends BasicDialog<BasicDialogProps, ShareDialogStats> {
 
     constructor(props: BasicDialogProps) {
         super(props)
+        const encodedUrl = window.location.href
         this.state = {
             title: document.title,
-            url: window.location.href
+            url: encodedUrl.endsWith("/") ? encodedUrl.substring(0, encodedUrl.length - 1) : encodedUrl
         }
     }
 
     componentDidMount(): void {
         super.componentDidMount()
         consoleDebug("ShareDialog componentDidMount, title = " + document.title + ", url = " + window.location.href)
-        const qrcodeImageE = this.rootE?.querySelector("#qrcode") as HTMLImageElement
+
         const qrcodeContainer = this.rootE?.querySelector(".share-qrcode-picture") as HTMLElement
 
         // 生成二维码，不要使用 svg，在浏览器中渲染时会出现点阵网格
@@ -59,15 +62,37 @@ class ShareDialog extends BasicDialog<BasicDialogProps, ShareDialogStats> {
         return (
             <div className="center-items">
                 <div className="share-qrcode-picture">
-                    {/* <img className="inline-for-center" alt="qrcode" /> */}
                 </div>
                 <p className="share-title">{this.state.title}</p>
-                <pre>
-                    <code className="share-url">{this.state.url}</code>
-                </pre>
+                <ShareUrlItem url={this.state.url} />
             </div>
         )
     }
+}
+
+interface ShareUrlItemProps {
+    url: string
+}
+
+function ShareUrlItem(props: ShareUrlItemProps) {
+    function onClickCopyUrl() {
+        navigator.clipboard.writeText(props.url).then(() => {
+            showSnackbar("已复制链接到剪贴板")
+            consoleDebug("Copy url success: " + props.url)
+        }).catch(err => {
+            showSnackbar("复制链接失败")
+            consoleDebug("Copy url failed: " + err)
+        })
+    }
+
+    return (
+        <div className="share-url-item">
+            <div className="share-url-span-wrapper">
+                <span className="share-url-span">{props.url}</span>
+            </div>
+            <IconButton icon="content_paste" onClick={onClickCopyUrl} className="btn-copy-url" />
+        </div>
+    )
 }
 
 let openCount = 0
--- npm/src/component/react/Button.tsx
+++ npm/src/component/react/Button.tsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react"
 import { clearFocusListener } from "../../util/tools"
 import { setupButtonRipple } from "../button"
 
-export interface Props {
+export interface BtnProps {
     text: string
     onClick: (() => void) | null;
     className: string;
 }
 
-export function Button(props: Props) {
+export function Button(props: BtnProps) {
     const containerRef = React.useRef<HTMLButtonElement>(null)
 
     useEffect(() => {
@@ -31,6 +31,38 @@ export function Button(props: Props) {
     return (
         <button ref={containerRef} type="button" className="mdc-button" tabIndex={-1}>
             <span className="mdc-button__label">{props.text}</span>
+        </button>
+    )
+}
+
+export interface IconBtnProps {
+    icon: string
+    onClick: (() => void) | null;
+    className: string;
+}
+
+export function IconButton(props: IconBtnProps) {
+    const containerRef = React.useRef<HTMLButtonElement>(null)
+
+    useEffect(() => {
+        const ele = containerRef.current as HTMLElement
+
+        setupButtonRipple(ele)
+        if (props.className != null && props.className.length > 0) {
+            const classes = props.className.split(" ")
+            classes.forEach((c) => {
+                ele.classList.add(c)
+            })
+        }
+        if (props.onClick != null) {
+            ele.addEventListener("click", props.onClick)
+        }
+        ele.addEventListener("focus", clearFocusListener)
+    }, [])
+
+    return (
+        <button ref={containerRef} type="button" className="mdc-icon-button" tabIndex={-1}>
+            <i className="material-symbols-rounded-light mdc-button__icon" aria-hidden="true">{props.icon}</i>
         </button>
     )
 }
