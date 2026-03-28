2f18ddc190238281b4fe658b72c6a088ea9e7dee
apqx <changgongapq@gmail.com>
2026-03-28 16:47:58 +0800

    Fix original image url parse issue

--- npm/src/page/post.ts
+++ npm/src/page/post.ts
@@ -50,26 +50,36 @@ function initImgJump() {
         // 点击图片，跳转到原图
         img.addEventListener("click", () => {
             const hasCopyright = img.classList.contains("operaCopyright")
-            // 所有的图片，缩略图都加了 _thumb 后缀，删除后即为原图
+            // 所有的图片的缩略图都加 _thumb 后缀，删除后即为原图
             // 图片只有 3 种格式，jpg、avif 和 webp
             // 命名示例：abcd_thumb.jpg abcd_thumb_jpg.webp abcd_thumb_for_lens_jpg.webp
             // _thumb: 缩略图标识
-            // _jpg: 原图后缀名标识，默认缩略图和原图为相同格式。若缩略图是 webp 原图是 jpg 需加此标识
-            // _avif: 原图后缀名标识，默认缩略图和原图为相同格式。若缩略图是 webp 原图是 avif 需加此标识
             // _for_lens: 透镜分区标识，表明此缩略图是为透镜分区准备的
             // _for_cover: 封面标识，表明此缩略图是为封面准备的
+            // 原图后缀名标识必须放在文件名尾部
+            // _jpg: 原图后缀名标识，默认缩略图和原图为相同格式。若缩略图是 webp 原图是 jpg 需加此标识
+            // _avif: 原图后缀名标识，默认缩略图和原图为相同格式。若缩略图是 webp 原图是 avif 需加此标识
             let imgUrl = img.getAttribute("src")!!
-            if (imgUrl.includes("_jpg")) {
-                imgUrl = imgUrl.substring(0, imgUrl.lastIndexOf(".")) + ".jpg"
-            } else if (imgUrl.includes("_avif")) {
-                imgUrl = imgUrl.substring(0, imgUrl.lastIndexOf(".")) + ".avif"
+            // 如果是缩略图，获取其原图 URL
+            if (imgUrl.includes("_thumb")) {
+                let urlWithoutSuffix = imgUrl.substring(0, imgUrl.lastIndexOf("."))
+                // 删除 thumb 之后尾部的原图后缀名标记
+                if (urlWithoutSuffix.endsWith("_jpg")) {
+                    imgUrl = urlWithoutSuffix.substring(0, urlWithoutSuffix.length - "_jpg".length) + ".jpg"
+                } else if (urlWithoutSuffix.endsWith("_avif")) {
+                    imgUrl = urlWithoutSuffix.substring(0, urlWithoutSuffix.length - "_avif".length) + ".avif"
+                } else if (urlWithoutSuffix.endsWith("_webp")) {
+                    imgUrl = urlWithoutSuffix.substring(0, urlWithoutSuffix.length - "_webp".length) + ".webp"
+                }
+                // 删除缩略图标记和缩略图用途标记
+                imgUrl = imgUrl.replace("_thumb", "").replace("_for_lens", "").replace("_for_cover", "")
             }
-            targetUrl = imgUrl.replace("_thumb", "").replace("_jpg", "").replace("_avif", "").replace("_for_lens", "").replace("_for_cover", "")
-            consoleDebug("Click show original img, copyright = " + hasCopyright + ", => " + targetUrl)
+            
+            consoleDebug("Click show original img, copyright = " + hasCopyright + ", => " + imgUrl)
             if (hasCopyright) {
-                showCopyrightDialog(targetUrl);
+                showCopyrightDialog(imgUrl);
             } else {
-                window.open(targetUrl, "_blank")
+                window.open(imgUrl, "_blank")
             }
         })
     }
