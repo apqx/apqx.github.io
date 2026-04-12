bf524144728e08f93709e1372c6c1082e2d413ea
apqx <changgongapq@gmail.com>
2026-04-13 00:31:22 +0800

    Refactor QRCode generation and fix color issues on xiaomi browser's dark mode

--- npm/src/component/dialog/ShareDialog.tsx
+++ npm/src/component/dialog/ShareDialog.tsx
@@ -4,6 +4,7 @@ import type { BaseDialogOpenProps } from "./BaseDialog"
 import { consoleDebug } from "../../util/log"
 import { useEffect, useMemo, useRef } from "react"
 import QRCodeStyling from "qr-code-styling"
+import QRCode from "qrcode-generator"
 import { showSnackbar } from "../react/Snackbar"
 import { IconButton } from "../react/Button"
 
@@ -22,31 +23,58 @@ function ShareDialog(props: BaseDialogOpenProps) {
     useEffect(() => {
         const qrcodeContainer = containerRef.current?.querySelector(".share-qrcode-picture") as HTMLElement
 
+        // 计算二维码尺寸，二维码的实际尺寸由模块数量、模块大小和边距组成
+        // 若不计算，生成的二维码 margin 可能不一致
+        // 虽然这样计算出的尺寸缩放到元素尺寸也会导致 margin 不一致，但至少在生成时是固定的，缩放后相差不大
+        let modules = getQRCodeModulesCount(url, "M")
+        let margin = 18
+        let moduleSize = 6
+
+        let qrCodeSize = modules * moduleSize + margin * 2
+
+        consoleDebug("QRCode calculate size: " + qrCodeSize + "px, margin: " + margin + "px")
+
+        if (qrCodeSize < 300) {
+            // 最小尺寸为 300x300，否则放大会出现毛边
+            qrCodeSize = 300
+             // 设置固定 margin
+            margin = 15
+            consoleDebug("QRCode calculate size < 300, use size: " + qrCodeSize + "px, margin: " + margin + "px")
+        }
+
         // 生成二维码，不要使用 svg，在浏览器中渲染时会出现点阵网格
-        const qrCode = new QRCodeStyling({
-            width: 300,
-            height: 300,
-            type: "canvas",
-            data: url,
-            image: "",
-            qrOptions: {
-                errorCorrectionLevel: "M"
-            },
-            cornersSquareOptions: {
-                type: "rounded",
-            },
-            dotsOptions: {
-                color: "#000000",
-                type: "extra-rounded"
-            },
-            backgroundOptions: {
-                color: "#ffffff",
-            },
-            imageOptions: {
-                crossOrigin: "anonymous",
-                margin: 140
+        const qrCode = new QRCodeStyling(
+            {
+                type: "canvas",
+                shape: "square",
+                width: qrCodeSize,
+                height: qrCodeSize,
+                data: url,
+                margin: margin,
+                qrOptions: {
+                    typeNumber: 0,
+                    mode: "Byte",
+                    errorCorrectionLevel: "M"
+                },
+                dotsOptions: {
+                    type: "extra-rounded",
+                    color: "#000000",
+                    roundSize: true
+                },
+                backgroundOptions: {
+                    round: 0,
+                    color: "#ffffff"
+                },
+                cornersSquareOptions: {
+                    type: "extra-rounded",
+                    color: "#000000"
+                },
+                cornersDotOptions: {
+                    type: "dot",
+                    color: "#000000"
+                }
             }
-        });
+        );
         qrCode.append(qrcodeContainer)
     }, [])
 
@@ -93,3 +121,12 @@ let openCount = 0
 export function showShareDialog() {
     showDialog(<ShareDialog openCount={openCount++} />, SHARE_DIALOG_WRAPPER_ID)
 }
+
+function getQRCodeModulesCount(data: string, errorCorrectionLevel: "L" | "M" | "Q" | "H" = "M") {
+    const qr = QRCode(0, errorCorrectionLevel); // 0 = auto version
+    qr.addData(data);
+    qr.make();
+
+    return qr.getModuleCount(); // e.g. 25 / 29 / 33 ...
+}
+
